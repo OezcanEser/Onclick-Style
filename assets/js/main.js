a6c49390e0f651e7ea9e246349feb53486cee206
OezcanEser <oe.eser84@gmail.com>
2021-05-06 16:25:42 +0200

    begin

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+function changeColor() {
+    document.getElementById('navHome').style.backgroundColor = "#EFA7CB";
+    document.getElementById('navHome').style.color = "#333";
+    document.getElementById('navHome').style.transform = "scaleX(-1)";
+}
