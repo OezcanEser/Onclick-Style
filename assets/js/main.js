5bbc5d02a41c2675c992a356e9ceccb3decd96d2
OezcanEser <oe.eser84@gmail.com>
2021-05-07 09:15:38 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function changeButton() {
-    document.getElementById('navHome').style.backgroundColor = "#EFA7CB";
-    document.getElementById('navHome').style.color = "#333";
-    document.getElementById('navHome').style.transform = "scaleX(-1)";
+    let bgColor = document.getElementById('navHome').style.backgroundColor = "#EFA7CB";
+    let color = document.getElementById('navHome').style.color = "#333";
+    let transform = document.getElementById('navHome').style.transform = "scaleX(-1)";
 }
